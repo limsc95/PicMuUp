41e5f1808a0f99ef899a224532827c55e95adacd
limsc <roko786@naver.com>
2025-07-24 14:24:43 +0900

    feat: 예외 처리 추가

---
+++ src/main/java/com/pmu/picmuup/exception/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.pmu.picmuup.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(InvalidRecommendRequestException.class)
+    public ResponseEntity<Map<String, String>> handleInvalidRecommendRequest(InvalidRecommendRequestException ex) {
+
+        return ResponseEntity.badRequest().body(Map.of(
+                "error", "잘못된 추천 요청", "message", ex.getMessage()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Map<String, String>> handleException(Exception ex) {
+        return ResponseEntity.badRequest().body(Map.of(
+                "error", "서버 오류", "message", ex.getMessage()
+        ));
+    }
+}
---
+++ src/main/java/com/pmu/picmuup/exception/InvalidRecommendRequestException.java
@@ -0,0 +1,7 @@
+package com.pmu.picmuup.exception;
+
+public class InvalidRecommendRequestException extends RuntimeException {
+    public InvalidRecommendRequestException(String message) {
+        super(message);
+    }
+}
